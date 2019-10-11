eb18c5b218adf5ce88aa06ab2bbcc0f04094c9d8
bensama <bentppaine@gmail.com>
2019-10-11 17:21:05 +0000

    fix

--- creativeproject2/script.js
+++ creativeproject2/script.js
@@ -1,43 +1,24 @@
-// document.getElementById("enterClass").addEventListener("submit", function(enterClass()));
-    
-// function enterClass() {
-//     var id = document.getElementById("index").value;
-//     var fullURL = "http://dnd5eapi.co/api/classes/" + id;
-
-//     console.log(fullURL);
-
-//     fetch(fullURL)
-//     .then ((resp) => resp.json())
-//     .then (function(data) {
-//         //let result = `<h2>Class Information</h2>`;
-//         console.log(data)
-//     });
-
-
-//     //document.getElementById('result').innerHTML = result;
-
-// }
-
-
-    
-
-document.getElementById("enterClass").addEventListener("submit", function(event) {
+document.getElementById("getData").addEventListener("click", function(event) {
     event.preventDefault();
-    var id = document.getElementById("index").value;
-    console.log("id is",id);
-    var fullURL = "http://dnd5eapi.co/api/classes/" + id;
-
-    fetch(fullURL, {mode:'no-cors'})
-    .then(response => {
-        console.log(response);
-        return response.json();
-    })
-    .then(data => {
-        let result = `<h2>class</h2>`; 
-        console.log(data);
-        
-    });
+    const index = document.getElementById("holidayInput").value;
+    if (index === "")
+      return;
+    console.log(index);
     
-    document.getElementById("result").innerHTML;
-  });
-
+    const url = "https://holidayapi.com/v1/holidays?key=eb712744-fe40-4d17-8ec6-a51606817502&country=US&year=2018&pretty";
+    
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {	
+        console.log(json);
+    
+        let results = "";
+      results += '<h2>' + json.holidays[index].name + '</h2>';
+      results += "<p>";
+      results += 
+      results += "</p>";
+      document.getElementById("results").innerHTML = results;
+      });
+      
+});
