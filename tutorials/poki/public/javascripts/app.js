30ea1bd0c3cef6ab3813ef9f4a8e152ec4026e6f
bensama <bentppaine@gmail.com>
2019-10-23 15:50:45 +0000

    poki

--- tutorials/poki/public/javascripts/app.js
+++ tutorials/poki/public/javascripts/app.js
@@ -4,11 +4,13 @@ var app = new Vue({
   el: '#app',
   data: {
     pokis: [],
+    politics:[],
     pokiName: '',
     pokiURL: '',
   },
   created: function() {
     this.getpokis();
+    this.getpolitics();
   },
   methods: {
     async getpokis() {
@@ -24,6 +26,35 @@ var app = new Vue({
       catch (error) {
         console.log(error);
       }
+    },
+     async getpolitics() {
+      // `this` points to the vm instance
+      console.log("politics");
+      var url = "http://class.benpaine.com:4200/politics";
+
+      try {
+        let response = await axios.get(url);
+        this.politics = response.data;
+        console.log(this.politics);
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    addItem() {
+      var url = "http://yourserver:4200/pokemon";
+      axios.post(url, {
+          name: this.pokiName,
+          avatarUrl: this.pokiURL
+        })
+        .then(response => {})
+        .catch(e => {
+          console.log(e);
+        });
+      this.pokiName = '';
+      this.pokiURL = '';
+      this.getpokis();
     },
   }
 });
