eb068a1f35b98ce0741de7bfd019eb6ce976f02b
Ben <bentppain@gmail.com>
2019-10-25 11:17:22 -0600

    last

--- creativeproject3/script.js
+++ creativeproject3/script.js
@@ -1,24 +1,41 @@
 var app = new Vue({
   el: '#app',
   data: {
-    // cities: [],
     number: '',
+    result: "",
   },
   methods: {
-    getClass() {
+    getInfo() {      
       console.log("Fetch " + this.number);
 
-      var url = "https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/classes/" + this.number;
+      var url = "https://cors-anywhere.herokuapp.com/http://numbersapi.com/" + this.number;
       console.log("URL " + url);
 
-      fetch(url)
-        .then((data) => {
-          return (data.json());
-        });
-        // .then((classlist) => {
-        //   console.log("Class Prof");
-        //   console.log(classlist);
-        // })
+      fetch(url, {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+  
+      })
+      .then(response => response.json())
+      .then(data => (this.result = data));
+    },
+    getRandom() {      
+      console.log("Fetch Random");
+
+      var url = "https://cors-anywhere.herokuapp.com/http://numbersapi.com/random";
+      console.log("URL " + url);
+
+      fetch(url, {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+  
+      })
+      .then(response => response.json())
+      .then(data => (this.result = data));
     },
   },
 });
