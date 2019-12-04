fffea27d64a1d999d7318c72a4a8050c4938c033
Ben <bentppain@gmail.com>
2019-12-04 10:36:13 -0700

    vueCLI

--- tutorials/full-stack/comment/app.js
+++ tutorials/full-stack/comment/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/comments');
+require('./models/Comments');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
